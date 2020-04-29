63ad81fa46af797417202e5aa29e8fa79e738efa
gracebrandolph <56567886+gracebrandolph@users.noreply.github.com>
2020-04-28 22:47:04 -0500

    Finsihed with cities studio

---
+++ src/Cities/Comparers/AreaComparer.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace Cities.Comparers
+{
+    public class AreaComparer : IComparer<City>
+    {
+        public int Compare(City x, City y)
+        {
+
+            return x.Area.CompareTo(y.Area);
+        }
+    }
+}
---
+++ src/Cities/Comparers/PopulationComparer.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace Cities.Comparers
+{
+    public class PopulationComparer : IComparer<City>
+    {
+        public int Compare(City x, City y)
+        {
+            
+            return x.Population.CompareTo(y.Population);
+        }
+    }
+}
